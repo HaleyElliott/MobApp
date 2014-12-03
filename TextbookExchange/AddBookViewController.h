be50993e248ac8460ad180f9308e6bcca23b49ff
jingxianfeng <jfeng34@asu.edu>
2014-12-03 01:09:17 -0700

    Image works for adding book

--- TextbookExchange/AddBookViewController.h
+++ TextbookExchange/AddBookViewController.h
@@ -4,10 +4,21 @@
 //
 //  Created by Haley Elliott on 11/3/14.
 //
+//  Modified By JINGXIAN FENG on 12/2/14 with image function.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface AddBookViewController : UIViewController
+@interface AddBookViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+{
+    UIImagePickerController *picker;
+    UIImagePickerController *pickerLib;
+    
+    UIImage *image;
+    __weak IBOutlet UIImageView *imageView;
+}
+- (IBAction)takePhoto:(id)sender;
+- (IBAction)choosePic:(id)sender;
+- (IBAction)clearImage:(id)sender;
 
 @end
--- TextbookExchange/AddBookViewController.m
+++ TextbookExchange/AddBookViewController.m
@@ -4,12 +4,14 @@
 //
 //  Created by Haley Elliott on 11/3/14.
 //
+//  Modified By JINGXIAN FENG on 12/2/14 with image function.
 //
 
 #import "AddBookViewController.h"
 #import <Parse/Parse.h>
 
 @interface AddBookViewController ()
+@property UIAlertView *alert;
 @property (weak, nonatomic) IBOutlet UITextField *authorTxt;
 @property (weak, nonatomic) IBOutlet UITextField *ISBNtxt;
 @property (weak, nonatomic) IBOutlet UITextField *titleTxt;
@@ -33,6 +35,10 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    // Show no-cover image
+    image = [UIImage imageNamed:@"book_cover_not_found.jpg"];
+    [imageView setImage:image];
 }
 
 - (void)didReceiveMemoryWarning
@@ -58,9 +64,15 @@
 }
 - (IBAction)AddBookBut:(UIButton *)sender {
     [super viewDidLoad];
+    
+    // Convert book cover image to NSData and then using PFFile
+    NSData *imageData = UIImagePNGRepresentation(image);
+    PFFile *imageFile = [PFFile fileWithName:@"image.jpg" data:imageData];
+    
     PFUser *currentUser = [PFUser currentUser];
     
     PFObject *book = [PFObject objectWithClassName:@"Books"];
+    book[@"imageFile"] = imageFile;
     book[@"ISBN"] = _ISBNtxt.text;
     book[@"ownerID"] = currentUser.username;
     book[@"title"] = _titleTxt.text;
@@ -76,6 +88,53 @@
     _ISBNtxt.text = @"";
     _authorTxt.text = @"";
     _titleTxt.text =@"";
+    image = [UIImage imageNamed:@"book_cover_not_found.jpg"];
+    [imageView setImage:image];    
+}
+
+- (IBAction)takePhoto:(id)sender {
+    // Determine if there is a camera
+    if ([UIImagePickerController isSourceTypeAvailable: UIImagePickerControllerSourceTypeCamera] == YES){
+        picker = [[UIImagePickerController alloc] init];
+        picker.delegate = self;
+        
+        // Set source to the camera
+        picker.sourceType =  UIImagePickerControllerSourceTypeCamera;
+        
+        // Show image picker
+        [self presentViewController:picker animated:YES completion:NULL];
+    }
+    else{
+        self.alert = [[UIAlertView alloc] initWithTitle:@"No Camera!" message:@"Sorry, you don't have a camera on this device." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [self.alert show];
+    }
+}
+
+- (IBAction)choosePic:(id)sender {
+    pickerLib = [[UIImagePickerController alloc] init];
+    pickerLib.delegate = self;
     
+    // Set source to the camera
+    pickerLib.sourceType =  UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    // Show image picker
+    [self presentViewController:pickerLib animated:YES completion:NULL];
+}
+
+- (void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    // Show image in the UIImageView
+    [imageView setImage:image];
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+- (void) imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+- (IBAction)clearImage:(id)sender {
+    image = [UIImage imageNamed:@"book_cover_not_found.jpg"];
+    [imageView setImage:image];
+    [self dismissViewControllerAnimated:YES completion:NULL];
 }
 @end
