85a77bccc6a5fa981bdb8b964111592c53547b17
HaleyElliott <hme.elliott@gmail.com>
2014-12-02 19:40:05 -0700

    More StoryBoard Changes

--- TextbookExchange/ChatViewController.h
+++ TextbookExchange/ChatViewController.h
@@ -9,5 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ChatViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITableView *tableChat;
+
+@property (weak, nonatomic) IBOutlet UITextField *chatField;
+@property (weak, nonatomic) IBOutlet UILabel *chatLable;
 
 @end
